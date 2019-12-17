fd4bd1d635a3911f37817fdecbb003a898657064
hrchu <petertc.chu@gmail.com>
2019-12-17 22:03:53 +0800

    Add doc and typing

--- pymogilefs/client.py
+++ pymogilefs/client.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict
 
 from pymogilefs import backend
 from pymogilefs.exceptions import FileNotFoundError, MogilefsError
@@ -24,14 +25,16 @@ class Client:
     def _create_close(self, **kwargs):
         return self._do_request(backend.CreateCloseConfig, **kwargs)
 
-    def get_file(self, key, timeout=None, zone='alt'):
+    def get_file(self, key, timeout=None, zone='alt') -> bytes:
         """
+        Given a key, returns a filehandle.
+
         Make sure to consume all the data so the connection could be closed.
 
-        :param key:
-        :param timeout:
+        @param key:
+        @param timeout:
         @param zone:
-        :return:
+        @return:
         """
         paths = self.get_paths(key, zone=zone).data
         if not paths['paths']:
@@ -48,15 +51,16 @@ class Client:
         #raise  # UnknownFileError
             raise Exception('No usable location to get file.')
 
-    def store_file(self, file_handle, key, _class=None, timeout=None, zone='alt'):
+    def store_file(self, file_handle, key, _class=None, timeout=None, zone='alt') -> Dict:
         """
+        Given a key, class, and a filehandle, stores the file contents in MogileFS.
 
         @param file_handle:
         @param key:
         @param _class:
         @param timeout:
         @param zone:
-        @return:
+        @return: path and length
         """
         kwargs = {'domain': self._domain,
                   'key': key,
@@ -99,6 +103,7 @@ class Client:
 
     def delete_file(self, key):
         """
+        Delete a key from MogileFS.
 
         @param key:
         @return:
@@ -107,21 +112,23 @@ class Client:
                                 domain=self._domain,
                                 key=key)
 
-    def rename_file(self):
+    def rename_file(self, key) -> bool:
         """
+        Rename file (key) in MogileFS from oldkey to newkey.
 
-        @return:
+        @return: true on success, failure otherwise.
         """
         raise NotImplementedError
 
-    def get_paths(self, key, noverify=True, zone='alt', pathcount=2):
+    def get_paths(self, key, noverify=True, zone='alt', pathcount=2) -> Response:
         """
+        Given a key, returns an array of all the locations (HTTP URLs) that the file has been replicated to.
 
         @param key:
-        @param noverify:
-        @param zone:
-        @param pathcount:
-        @return:
+        @param noverify: If the "no verify" option is set, the mogilefsd tracker doesn't verify that the first item returned in the list is up/alive. Skipping that check is faster, so use "noverify" if your application can do it faster/smarter. For instance, when giving Perlbal a list of URLs to reproxy to, Perlbal can intelligently find one that's alive, so use noverify and get out of mod_perl or whatever as soon as possible.
+        @param zone: If the zone option is set to 'alt', the mogilefsd tracker will use the alternative IP for each host if available, while constructing the paths.
+        @param pathcount: If the pathcount option is set to a positive integer greater than 2, the mogilefsd tracker will attempt to return that many different paths (if available) to the same file. If not present or out of range, this value defaults to 2.
+        @return: Response within paths and path_count
         """
         # TODO: timeout?
         return self._do_request(backend.GetPathsConfig,
@@ -131,13 +138,14 @@ class Client:
                                 zone=zone,
                                 pathcount=pathcount)
 
-    def list_keys(self, prefix=None, after=None, limit=None):
+    def list_keys(self, prefix=None, after=None, limit=None) -> Response:
         """
+        Used to get a list of keys matching a certain prefix.
 
-        @param prefix:
-        @param after:
-        @param limit:
-        @return:
+        @param prefix: specifies what you want to get a list of.
+        @param after: the item specified as a return value from this function last time you called it.
+        @param limit: defaults to 1000 keys returned.
+        @return: Response within key_count, next_after, and keys.
         """
         kwargs = {'domain': self._domain}
         if prefix is not None:
